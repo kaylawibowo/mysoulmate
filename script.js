b64859871047d0f6456375108e3648e766036278
kaylawibowo <gd.kayla.wibowo@gmail.com>
2026-08-01 22:19:18 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function showGift(){
+
+document.getElementById("cover").style.display="none";
+
+document.getElementById("gift").classList.remove("hidden");
+
+}
+
+function showLetter(){
+
+document.getElementById("gift").style.display="none";
+
+document.getElementById("letterPage").classList.remove("hidden");
+
+}
