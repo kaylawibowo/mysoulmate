0cd42a5b2cb68d667f533ce52d8d38ee8b4251f1
kaylawibowo <gd.kayla.wibowo@gmail.com>
2026-08-01 23:44:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,19 @@ setInterval(() => {
     },7000);
 
 },500);
+for(let i=0;i<30;i++){
+
+    const star=document.createElement("div");
+
+    star.classList.add("star");
+
+    star.innerHTML="✦";
+
+    star.style.left=Math.random()*100+"vw";
+    star.style.top=Math.random()*100+"vh";
+
+    star.style.fontSize=(Math.random()*15+10)+"px";
+
+    document.body.appendChild(star);
+
+}
