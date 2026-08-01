eaafd962fdeaa6de8b66bff02faabbe1f1ecd135
kaylawibowo <gd.kayla.wibowo@gmail.com>
2026-08-01 23:17:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ document.getElementById("gift").style.display="none";
 document.getElementById("letterPage").classList.remove("hidden");
 
 }
+function openGift(){
+
+    const gift = document.querySelector(".gift-box");
+    const button = document.getElementById("letterButton");
+
+    gift.classList.add("open");
+
+    setTimeout(()=>{
+        button.classList.remove("hidden");
+    },800);
+
+}
+function showLetter(){
+
+    alert("💌 Happy Birthday Niwniw 🤍");
+
+}
